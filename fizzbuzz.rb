a05f69d95865bcd47396f57d9f78fb827183b1eb
lapeyrem19 <lapeyrem19@ashevilleschool.org>
2018-06-05 19:52:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,13 @@
 def fizzbuzz(int)
   if int % 3 == 0
+    puts "Fizz"
+  elsif int % 5 == 0
+    puts "Buzz"
+  elsif int % 3 == 0 && int % 5 == 0
+  puts "Fizzbuzz"
   end
 end 
+
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
